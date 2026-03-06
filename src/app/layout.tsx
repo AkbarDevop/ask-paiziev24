054239d70876a92791ee7c954e1299d7924ec764
Akbar Kamoldinov <149901115+AkbarDevop@users.noreply.github.com>
2026-03-06 08:55:35 -0600

    Switch askpaiziev app to new GTM container

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,7 +62,7 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-MCJXDNSX');`}
+})(window,document,'script','dataLayer','GTM-K2CFJGHR');`}
         </Script>
         <script
           defer
@@ -73,7 +73,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       <body className={`${inter.variable} font-sans antialiased`}>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-MCJXDNSX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K2CFJGHR"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
